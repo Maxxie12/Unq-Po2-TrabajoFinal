445d2d18101e67ed957ce6ce7293aa10a9833cce
Max_xie <Max_xie@192.168.0.85>
2023-06-12 19:41:56 -0300

    Actualizado el buscador por nivel de verificacion

--- src/ar/edu/unq/po2/BuscadorDeMuestras/BusquedaNivelVerificacion.java
+++ src/ar/edu/unq/po2/BuscadorDeMuestras/BusquedaNivelVerificacion.java
@@ -3,13 +3,14 @@ package ar.edu.unq.po2.BuscadorDeMuestras;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import ar.edu.unq.po2.Muestra.EstadoMuestra;
 import ar.edu.unq.po2.Muestra.Muestra;
 
 public class BusquedaNivelVerificacion implements IBuscadorMuestras{
 	
-	private String estadoMuestraABuscar;
+	private EstadoMuestra estadoMuestraABuscar;
 
-	    public void setEstadoMuestraABuscar(String estadoMuestraABuscar) {
+	    public void setEstadoMuestraABuscar(EstadoMuestra estadoMuestraABuscar) {
 	        this.estadoMuestraABuscar = estadoMuestraABuscar;
 	    }
 
