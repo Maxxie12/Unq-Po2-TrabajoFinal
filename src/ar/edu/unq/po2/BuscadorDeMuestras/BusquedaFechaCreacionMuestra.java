3dfea483b0b743508b5af53f18571aea4ea195ce
Max_xie <Max_xie@192.168.0.85>
2023-06-12 18:11:42 -0300

    Restaurada la busqueda de fecha de creacion

---
+++ src/ar/edu/unq/po2/BuscadorDeMuestras/BusquedaFechaCreacionMuestra.java
@@ -0,0 +1,29 @@
+package ar.edu.unq.po2.BuscadorDeMuestras;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import ar.edu.unq.po2.Muestra.Muestra;
+
+public class BusquedaFechaCreacionMuestra implements IBuscadorMuestras {
+
+		LocalDate fecha;
+
+		public List<Muestra> filtrar(List<Muestra> muestrasAFiltrar) {
+			List<Muestra> resultado = new ArrayList<>();
+			resultado = muestrasAFiltrar.stream()
+					.filter(muestra -> muestra.getFechaCreacion().isAfter(this.fecha))
+	                .collect(Collectors.toList());
+			return (resultado);				
+		}
+
+
+		public void setFecha(LocalDate fecha) {
+			this.fecha = fecha;
+		}
+
+
+
+}
