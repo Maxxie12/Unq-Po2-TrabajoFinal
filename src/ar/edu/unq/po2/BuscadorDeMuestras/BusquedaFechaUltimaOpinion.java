bbcc0da9576b28d355c7a1ff921f554254479f4a
Max_xie <Max_xie@192.168.0.85>
2023-06-12 19:37:47 -0300

    Creado el buscador por fechas ultima opinion

--- src/ar/edu/unq/po2/BuscadorDeMuestras/BusquedaFechaUltimaOpinion.java
+++ src/ar/edu/unq/po2/BuscadorDeMuestras/BusquedaFechaUltimaOpinion.java
@@ -1,5 +1,28 @@
 package ar.edu.unq.po2.BuscadorDeMuestras;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import ar.edu.unq.po2.Muestra.Muestra;
+
 public class BusquedaFechaUltimaOpinion implements IBuscadorMuestras {
+	
+	private LocalDate fecha;
+
+	public List<Muestra> filtrar(List<Muestra> muestrasAFiltrar) {
+		 return muestrasAFiltrar.stream()
+	                .filter(muestra -> muestra.getOpiniones().stream()
+	                        .anyMatch(opinion -> opinion.getFechaPublicacion().isAfter(fecha)))
+	                .collect(Collectors.toList());
+	    }
+		
+
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
 
 }
