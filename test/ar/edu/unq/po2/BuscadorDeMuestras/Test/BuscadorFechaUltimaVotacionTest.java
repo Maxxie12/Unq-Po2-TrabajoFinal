d44b944267bef7f8116adaa3d6f9579f6f4fd3bc
Max_xie <Max_xie@192.168.0.85>
2023-06-13 19:37:25 -0300

    Implementado el test de buscado por tipo bicho

--- test/ar/edu/unq/po2/BuscadorDeMuestras/Test/BuscadorFechaUltimaVotacionTest.java
+++ test/ar/edu/unq/po2/BuscadorDeMuestras/Test/BuscadorFechaUltimaVotacionTest.java
@@ -1,5 +1,22 @@
 package ar.edu.unq.po2.BuscadorDeMuestras.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+
+import ar.edu.unq.po2.BuscadorDeMuestras.BuscadorFechaUltimaVotacion;
+import ar.edu.unq.po2.Muestra.Muestra;
+import ar.edu.unq.po2.Muestra.Opinion;
+
 public class BuscadorFechaUltimaVotacionTest {
+	
+    
 
 }
--- test/ar/edu/unq/po2/BuscadorDeMuestras/Test/BuscadorTipoIntectoTest.java
+++ test/ar/edu/unq/po2/BuscadorDeMuestras/Test/BuscadorTipoIntectoTest.java
@@ -1,5 +1,72 @@
 package ar.edu.unq.po2.BuscadorDeMuestras.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+
+import ar.edu.unq.po2.BuscadorDeMuestras.BuscadorTipoInsecto;
+import ar.edu.unq.po2.Muestra.Muestra;
+import ar.edu.unq.po2.Muestra.OpinionImagen;
+
 public class BuscadorTipoIntectoTest {
+	
+	List<Muestra> muestrasAFiltrar = new ArrayList<Muestra>();
+	BuscadorTipoInsecto buscadorTipoInsecto;  
+	
+	
+		@Mock
+		  Muestra  muestra1 = mock(Muestra.class);
+		  Muestra  muestra2 = mock(Muestra.class);
+		  Muestra  muestra3 = mock(Muestra.class);
+	      Muestra  muestra4 = mock(Muestra.class);
+	
+	 
+		@BeforeEach
+	    void setUp() { 
+			when(muestra1.getTipoInsecto()).thenReturn(OpinionImagen.VINCHUCA_GUASAYANA);
+		  	when(muestra2.getTipoInsecto()).thenReturn(OpinionImagen.VINCHUCA_INFESTANS);
+		  	when(muestra3.getTipoInsecto()).thenReturn(OpinionImagen.CHINCHE_FOLIADA);
+		  	when(muestra4.getTipoInsecto()).thenReturn(OpinionImagen.VINCHUCA_SORDIDA);
+		    
+		
+	        muestrasAFiltrar.add(muestra1);
+			muestrasAFiltrar.add(muestra2);     
+			muestrasAFiltrar.add(muestra3);
+			muestrasAFiltrar.add(muestra4);
+			
+			buscadorTipoInsecto = new BuscadorTipoInsecto();
+	    }
+	   
+	    @Test
+	    void testBuscadorTipoBichoVinchuca(){   
+	    	
+	    	buscadorTipoInsecto.setInsectoABuscar("Vinchuca");
+	    	
+	    	assertTrue(buscadorTipoInsecto.filtrar(muestrasAFiltrar).contains(muestra2));
+	    	assertTrue(buscadorTipoInsecto.filtrar(muestrasAFiltrar).contains(muestra1));
+	    	assertFalse(buscadorTipoInsecto.filtrar(muestrasAFiltrar).contains(muestra3));
+	    	assertTrue(buscadorTipoInsecto.filtrar(muestrasAFiltrar).contains(muestra4));
+	    }
+
+	    @Test
+	    void testBuscadorTipoBichoCucaracha(){
+	    	buscadorTipoInsecto.setInsectoABuscar("Cucaracha");
+
+	    	assertFalse(buscadorTipoInsecto.filtrar(muestrasAFiltrar).size()>0);
+	    }
+		 
+		 
+
+		
+		
+	 }
 
 }
